1a1df2897afe7433894b62da021e87757a5608e6
Jaanus Siim <jaanus@jaanussiim.com>
2014-03-08 10:33:59 +0200

    Added license and podspec

---
+++ CDYFeedback.podspec
@@ -0,0 +1,15 @@
+Pod::Spec.new do |spec|
+  spec.name         = 'CDYFeedback'
+  spec.version      = '0.1.0'
+  spec.summary      = "Utility classes for user feedback."
+  spec.homepage     = "https://github.com/coodly/CDYFeedback"
+  spec.author       = { "Jaanus Siim" => "jaanus@coodly.com" }
+  spec.source       = { :git => "https://github.com/coodly/CDYFeedback.git", :tag => "v#{spec.version}" }
+  spec.license      = { :type => 'Apache 2', :file => 'LICENSE.txt' }
+  spec.requires_arc = true
+
+  spec.subspec 'Core' do |ss|
+    ss.platform = :ios, '7.0'
+    ss.source_files = 'Core/*.{h,m}'
+  end
+end
--- FeedbackSampleTests/FeedbackSampleTests.m
+++ FeedbackSampleTests/FeedbackSampleTests.m
@@ -1,10 +1,18 @@
-//
-//  FeedbackSampleTests.m
-//  FeedbackSampleTests
-//
-//  Created by Jaanus Siim on 08/03/14.
-//  Copyright (c) 2014 Coodly LLC. All rights reserved.
-//
+/*
+ * Copyright 2014 Coodly LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <XCTest/XCTest.h>
 
@@ -14,20 +22,17 @@
 
 @implementation FeedbackSampleTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
+- (void)testExample {
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
 
